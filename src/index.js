e38321f9ada4c2d9eb6ab1899f744d0f0de85a43
juanganteng <juan.alvito@student.umn.ac.id>
2020-12-14 21:11:15 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router> 
+        <Switch>
+          <Route exact path="/">
+            <App />
+          </Route>
+        </Switch>
+      
+    </Router>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
